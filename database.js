2974c42434aee230a00f90c9404f1f18948930a4
druser0 <pandabotpanel@atomicmail.io>
2026-07-18 05:32:12 +0330

    Implement user management functions in database.js

--- database.js
+++ database.js
@@ -1,1 +1,19 @@
+export const db = {
+  users: new Map(),
 
+  addUser(user) {
+    this.users.set(user.id, user);
+  },
+
+  getUser(id) {
+    return this.users.get(id);
+  },
+
+  getUsers() {
+    return [...this.users.values()];
+  },
+
+  totalUsers() {
+    return this.users.size;
+  }
+};
