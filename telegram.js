b2d06a5d6438f16d9792e9f672b0f93ee0dd7e31
druser0 <pandabotpanel@atomicmail.io>
2026-07-18 05:31:03 +0330

    Add functions to send messages and get chat members

--- telegram.js
+++ telegram.js
@@ -1,1 +1,29 @@
+const API = `https://api.telegram.org/bot${BOT_TOKEN}`;
 
+export async function sendMessage(chatId, text, keyboard = null) {
+  const body = {
+    chat_id: chatId,
+    text,
+    parse_mode: "HTML"
+  };
+
+  if (keyboard) {
+    body.reply_markup = keyboard;
+  }
+
+  return fetch(`${API}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  });
+}
+
+export async function getChatMember(channel, userId) {
+  const res = await fetch(
+    `${API}/getChatMember?chat_id=${channel}&user_id=${userId}`
+  );
+
+  return await res.json();
+}
